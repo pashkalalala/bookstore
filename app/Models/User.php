888237f0de25f841121f289d53750a79d4ae2365
Pavlo Roshko <pasharoshko100@gmail.com>
2023-08-08 23:58:22 +0300

    Add phone column to User model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
     ];
 
@@ -42,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
