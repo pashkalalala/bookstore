9f0d6ccb95a8801a1b4918a9863a1a1792ea3c54
Pavlo Roshko <pasharoshko100@gmail.com>
2023-08-13 02:03:31 +0300

    Implement User Roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -47,5 +50,10 @@ class User extends Authenticatable
     public function orders()
     {
         return $this->hasMany(Order::class);
+    }
+
+    public function canAccessFilament(): bool
+    {
+        return $this->hasRole('admin');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,17 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductSeeder::class);
         $this->call(StatusSeeder::class);
 
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         \App\Models\Order::factory(20)->create();
+
+        /** @var User $adminUser */
+        $adminUser = User::factory()->create([
+            'email' => 'pasha@gmail.com',
+            'name' => 'Admin',
+            'password' => bcrypt('1111'),
+            'phone' => '+380634444555'
+        ]);
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminUser->assignRole($adminRole);
     }
 }
